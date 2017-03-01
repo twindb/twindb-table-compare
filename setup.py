a4b0946c683901149880f6f55e07f3156f6a2e23
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2017-02-28 18:08:17 -0800

    Bump version: 1.1.1 → 1.1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = [str(ir.req) for ir in
 
 setup(
     name='twindb_table_compare',
-    version='1.1.1',
+    version='1.1.2',
     description="TwinDB Table Compare reads percona.checksums from the master "
                 "and slave and shows what records are difference "
                 "if there are any inconsistencies.",
